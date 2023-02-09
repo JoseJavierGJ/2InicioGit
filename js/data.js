62e5722732d845c1529b1cf2049d464797abc6d4
JoseJavierGJ <jjosejavier@hotmail.com>
2023-02-09 10:17:38 -0600

    Ya acabamos

---
+++ js/data.js
@@ -0,0 +1,33 @@
+const songs = [
+    {
+        name: 'song 1',
+        path: './music/song 1.mp3',
+        artist: 'Los Topollos',
+        cover: '../images/cover 1.png'
+    },
+    {
+        name: 'song 2',
+        path: './music/song 2.mp3',
+        artist: 'Las Conejitas de Pilines',
+        cover: '../images/cover 2.png'
+    },
+    {
+        name: 'song 3',
+        path: './music/song 3.mp3',
+        artist: 'Leche Liconsa',
+        cover: '../images/cover 3.png'
+    },
+    {
+        name: 'song 4',
+        path: './music/song 4.mp3',
+        artist: 'Nintendo 3DS',
+        cover: '../images/cover 4.png'
+    },
+    {
+        name: 'song 5',
+        path: './music/song 4.mp3',
+        artist: 'Batman',
+        cover: '../images/cover 4.png'
+    },
+    
+]
